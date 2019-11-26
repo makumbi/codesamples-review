9b753a42f321e8d4eabe2bf7c552d7c545a2a0e2
Alex Makumbi <alexmakumbi15@gmail.com>
2019-11-25 20:41:22 -0500

    added prepared statements

--- example_1/1.java
+++ example_1/1.java
@@ -1,7 +1,35 @@
-String query = "SELECT account_balance FROM user_data WHERE user_name = "
-               + request.getParameter("customerName");
+
+// Implements Valation Controls
+String customerName = inputValidation(request.getParameter("customerName"));
 
 try {
-        Statement statement = connection.createStatement();
-        ResultSet results = statement.executeQuery(query);
+    // Implements Prepared Statements
+    PreparedStatement statement = connection.prepareStatement("SELECT account_balance FROM user_data WHERE user_name = ?");  
+    statement.setString(1, customerName);  
+    ResultSet results = statement.executeUpdate(); 
+} catch (SQLExecption ex) {
+        // Exception handling stuff
+} finally {
+    if (rs != null) {
+        try {
+            results.close();
+        } catch (SQLException e) { 
+            /* ignored */
+        }
+    }
+    if (ps != null) {
+        try {
+            statement.close();
+        } catch (SQLException e) { 
+            /* ignored */
+        }
+    }
+    if (connection != null) {
+        try {
+            connection.close();
+        } catch (SQLException e) { 
+            /* ignored */
+        }
+    }
 }
+

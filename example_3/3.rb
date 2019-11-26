8f4652907b97e942e54264ce9bd6c35c686235d6
Alex Makumbi <alexmakumbi15@gmail.com>
2019-11-26 00:22:00 -0500

    Implemented authorization and validation controls

--- example_3/3.rb
+++ example_3/3.rb
@@ -8,14 +8,22 @@ class MessagesController < ApplicationController
   end
 
   def show
+    # Validate input retrieved from parameter 'id'
+    validateId(params[:id].first)
     @message = Message.where(:id => params[:id]).first
   end
 
   def destroy
+    # Is the user authorized to delete the message?
+    # Implement an authorization control
+    isUserAuthorized(current_user)
+    # Validates id
+    validateId(params[:id].first)
     message = Message.where(:id => params[:id]).first
 
     if message.destroy
       flash[:success] = "Your message has been deleted."
+      # Ensure user_id is validated
       redirect_to user_messages_path(:user_id => current_user.user_id)
     else
       flash[:error] = "Could not delete message."
@@ -23,13 +31,20 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # Implement an authorization control
+    isUserAuthorized(current_user)
+
     if Message.create(message_params)
-      respond_to do |format|
+      respond_to do |format
+        # Validates user_id
+        validation(current_user.user_id)
         format.html { redirect_to user_messages_path(:user_id => current_user.user_id) }
         format.json { render :json => {:msg => "success"} }
       end
     else
       respond_to do |format|
+        # validates users_message_path
+        validation(user_messages_path)
         format.html { redirect_to user_messages_path }
         format.json { render :json => {:msg => "failure"} }
       end
@@ -39,6 +54,11 @@ class MessagesController < ApplicationController
   private
 
   def message_params
+    # Validation controls
+    validateId(:creator_id)
+    validateMessage(:message)
+    validateRead(:read)
+    validateReceiverId(:receiver_id)
     params.require(:message).permit(:creator_id, :message, :read, :receiver_id)
   end
 end
